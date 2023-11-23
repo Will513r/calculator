e9f41a920ba992eff32197acc3a7e79ded414878
Will <will513r@gmail.com>
2023-11-23 15:23:28 -0500

    working on functions
    Added the del function as well as changed the input area for better visability

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 let displayValue = ' ';
+const deleteButton = document.getElementById("delete-button");
+const addButton = document.getElementById("add-button");
+const subtractButton = document.getElementById("subtract-button");
+// 
+
+
 
 function updateDisplay(value) {
   const display = document.getElementById("display");
@@ -8,4 +14,37 @@ function updateDisplay(value) {
 function appendToDisplay(value) {
   displayValue += value;
   updateDisplay();
+}
+
+deleteButton.addEventListener("click", function appendToDisplay(value) {
+  displayValue -= value;
+  updateDisplay();
+});
+
+function deleteCharacter() {
+  var input = document.getElementById("display");
+  var currentValue = input.value;
+
+  if (currentValue.length > 0) {
+  var newValue = currentValue.substring(0, currentValue.length - 1);
+  input.value = newValue;
+  }
+}
+
+
+
+function clearDisplay() {
+  displayValue = " ";
+  updateDisplay();
+}
+
+function calculate() {
+  try {
+    const result = eval(displayValue);
+    displayValue = result.toString();
+    updateDisplay();
+  } catch (error) {
+    displayValue = "Error";
+    updateDisplay();
+  }
 }

cae77371a04d11f780d5b1bfaf288eeed38fd2f4
Will Germany <germany.william@gmail.com>
2023-11-26 21:53:11 -0500

    Applied fix for delete button causing display values to go missing

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ let displayValue = ' ';
 const deleteButton = document.getElementById("delete-button");
 const addButton = document.getElementById("add-button");
 const subtractButton = document.getElementById("subtract-button");
-// 
 
-
-
-function updateDisplay(value) {
+function updateDisplay() {
   const display = document.getElementById("display");
   display.textContent = displayValue;
 }
@@ -16,19 +13,16 @@ function appendToDisplay(value) {
   updateDisplay();
 }
 
-deleteButton.addEventListener("click", function appendToDisplay(value) {
-  displayValue -= value;
-  updateDisplay();
-});
-
 function deleteCharacter() {
   var input = document.getElementById("display");
   var currentValue = input.value;
 
   if (currentValue.length > 0) {
-  var newValue = currentValue.substring(0, currentValue.length - 1);
-  input.value = newValue;
+    var newValue = currentValue.substring(0, currentValue.length - 1);
+    displayValue = newValue;
   }
+
+  updateDisplay();
 }
 
 
@@ -39,6 +33,10 @@ function clearDisplay() {
 }
 
 function calculate() {
+  if (displayValue === " ") {
+    return;
+  }
+
   try {
     const result = eval(displayValue);
     displayValue = result.toString();

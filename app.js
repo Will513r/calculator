eecce6b6d3b8ec9fb3e4e42426cce1c71e6ecdd5
Will <will513r@gmail.com>
2023-11-27 14:13:52 -0500

    sound clip
    Added sound clip to the hello button

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ let displayValue = ' ';
 const deleteButton = document.getElementById("delete-button");
 const addButton = document.getElementById("add-button");
 const subtractButton = document.getElementById("subtract-button");
-
+const playButton = document.getElementById("hello");
+const voiceClip = document.getElementById("voiceClip");
 function updateDisplay() {
   const display = document.getElementById("display");
   display.textContent = displayValue;
@@ -25,7 +26,14 @@ function deleteCharacter() {
   updateDisplay();
 }
 
-
+function playClip(){
+   if (voiceClip.paused || voiceClip.ended) {
+     voiceClip.play(); 
+   } else {
+     voiceClip.pause(); 
+     voiceClip.currentTime = 0; 
+   }
+}
 
 function clearDisplay() {
   displayValue = " ";
